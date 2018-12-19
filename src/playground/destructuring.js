af83b507130e59a76dbe10a2566d978ec6377ef8
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-12-19 16:18:25 -0500

    Learn object destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -45,5 +45,5 @@ const book = {
   }
 }
 
-const {name: publisherName = 'Self-Published'} = book.publisher
+const { name: publisherName = 'Self-Published' } = book.publisher
 console.log(publisherName);
