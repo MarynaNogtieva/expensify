86e1b27f10bf4e3cd93183ef8211b17b1db99fbf
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2019-01-04 16:58:05 -0500

    list actual expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,14 +5,18 @@ const ExpenseList = (props) => {
   return (
     <div>
       <h1>Expense List</h1>
-      { props.name }
+      {
+        props.expenses.map((expense) => {
+          return <p key={ expense.id }>{ expense.description }</p>
+        })
+      }
     </div>
   )
 };
 
 const ConnectedExpenseList = connect((state) => {
   return {
-    name: 'coffee'
+    expenses: state.expenses
   };
 })(ExpenseList);
 export default ConnectedExpenseList;
