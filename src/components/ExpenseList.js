f7409f373b964aaa05958762b7ca93577adb18b4
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2019-01-04 16:58:35 -0500

    Use object destructuring

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-const ExpenseList = (props) => {
+const ExpenseList = ({expenses}) => {
   return (
     <div>
       <h1>Expense List</h1>
       {
-        props.expenses.map((expense) => {
+        expenses.map((expense) => {
           return <p key={ expense.id }>{ expense.description }</p>
         })
       }
