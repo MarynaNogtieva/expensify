79a1ec2a89ed6c6490d6ccab48dbd0e5f3da401f
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2019-01-05 23:03:00 -0500

    Write to the store via input

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashBoardPage = () => {
   return(
    <div>
+    <ExpenseListFilters />
     <ExpenseList />
    </div>
   );
