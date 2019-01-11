1fd2546d7bd6fdc2f9f5480a372528bf2ac594d8
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2019-01-11 16:56:22 -0500

    Add edit page

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
+
 
 const EditPage = (props) => {
-  console.log(props);
+  const id = props.match.params.id;
   return(
     <div>
-    this is from my EditPage component of {props.match.params.id}
+    <ExpenseForm
+      expense={props.expense}
+      onSubmit={(expense) => {
+        props.dispatch(editExpense(props.match.params.id, expense));
+        props.history.push('/');
+      }}
+    />
+    <button onClick={() => {
+      props.dispatch(removeExpense({ id }));
+      props.history.push('/');
+    }}>Remove</button>
    </div>
   )
 };
 
-export default EditPage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => {
+      return expense.id === props.match.params.id
+    })
+  }
+};
+
+export default connect(mapStateToProps)(EditPage);
