0079d83c54d6469c781bf72e3d12ca082e9dfc57
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2019-01-15 21:31:21 -0500

    Test header with enzyme and jest

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
+// test('should render Header correctly', () => {
+//   const renderer = new ReactShallowRenderer();
+//   renderer.render(<Header />);
+//   expect(renderer.getRenderOutput()).toMatchSnapshot()
+// });
+
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot()
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find('h1').length).toBe(1);
+  expect(wrapper.find('h1').text()).toBe('Expensify');
+  // we added configuration in jet.config.json to avoid using toJson here
+  // expect(toJson(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
 
 
