c84b05da28eef90afffc7a2c1d8f862d09184ebe
MarynaNogtieva <marynanogtieva@yahoo.ca>
2019-01-26 19:34:27 -0500

    Separate loading CSS files with webpack

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
 import { addExpense } from './actions/expenses';
-import { setTextFilter, setEndDate, setStartDate } from './actions/filters';
 
 import  getVisibleExpenses  from './selectors/expenses';
 
