5884ec637f3df57385b1739cf5bf567ad02ba92a
MarynaNogtieva <marynanogtieva@yahoo.ca>
2019-02-03 19:18:01 -0500

    Set up secrets properly for Firebase

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,10 +1,9 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve('This is my resolved data');
+    resolve('This is my resolved data');
 
-    reject('Something went wrong');
+    // reject('Something went wrong');
   }, 1500);
-
 });
 
 console.log('before');
@@ -14,6 +13,14 @@ promise.then((data) => {
 
 promise.then((data) => {
   console.log('2', data);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my OTHER resolved data');
+    }, 1500);
+  });  
+}).then((str) => {
+  console.log('does this run ', str);
 }).catch((error) => {
   console.log('error: ', error);
 });
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase'
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -49,13 +50,48 @@ test('should add expense to database and store', (done) => {
 
   // run async function
   store.dispatch(startAddExpense(expenseData)).then(() => {
-    expect(1).toBe(1);
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    });
+    // return a promise in order to create a promise chain
+    return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val()).toEqual(expenseData);
     done();
   });
 });
 
-test('should add expense with defaults to database and store', () => {
-
+test('should add expense with defaults to database and store', (done) => {
+   // redux-mock-store
+   const store = createMockStore({});
+   const expenseData = {
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0
+   }
+ 
+   // run async function
+   store.dispatch(startAddExpense({})).then(() => {
+     const actions = store.getActions();
+     expect(actions[0]).toEqual({
+       type: 'ADD_EXPENSE',
+       expense: {
+         id: expect.any(String),
+         ...expenseData
+       }
+     });
+     // return a promise in order to create a promise chain
+     return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+   }).then((snapshot) => {
+     expect(snapshot.val()).toEqual(expenseData);
+     done();
+   });
 });
 // test('shoudl setup add expense action object with default values', ()=> {
 //   const action = addExpense();
