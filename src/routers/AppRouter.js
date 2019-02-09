83ffc1f98557852cc9ab4856a4e7ead4deca1731
MarynaNogtieva <marynanogtieva@yahoo.ca>
2019-02-09 14:36:55 -0500

    Fix router and header for private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,25 +4,24 @@ import React from 'react';
 import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage.js';
 import EditPage from '../components/EditPage';
-import Header from '../components/Header';
+
 import HelpPage from '../components/HelpPage';
 import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage';
 import NotFoundPage from '../components/NotFoundPage';
 import  LoginPage from '../components/LoginPage';
 import { Router, Route, Switch }
  from 'react-router-dom';
-
+import PrivateRoute from './PrivateRoute';
 
  export const history = createHistory();
  const AppRouter = () => (
   <Router history={history}>
   <div>
-   <Header />
    <Switch>
      <Route path="/" component={ LoginPage } exact={true}/>
-     < Route path="/dashboard" component={ ExpenseDashBoardPage } />
-     <Route path='/create' component={ AddExpensePage } />
-     <Route path="/edit/:id" component={ EditPage } />
+     <PrivateRoute path="/dashboard" component={ ExpenseDashBoardPage } />
+     <PrivateRoute path='/create' component={ AddExpensePage } />
+     <PrivateRoute path="/edit/:id" component={ EditPage } />
      <Route path='/help' component={ HelpPage }  />
      <Route component={ NotFoundPage } />
    </Switch>
